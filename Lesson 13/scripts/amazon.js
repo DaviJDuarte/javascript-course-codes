045912b4dde7f25e66228cf1575adc914a847a20
Davi Duarte <101777522+DaviJDuarte@users.noreply.github.com>
2023-07-22 19:58:27 +0000

    Add syntax shorcuts to amazon.js

--- Lesson 13/scripts/amazon.js
+++ Lesson 13/scripts/amazon.js
@@ -59,7 +59,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
+      const { productId } = button.dataset;
       const quantity = Number(document.querySelector(
         `.js-quantity-selector-${productId}`
       ).value);
@@ -76,8 +76,8 @@ document.querySelectorAll('.js-add-to-cart')
         matchingItem.quantity += quantity;
       } else {
         cart.push({
-          productId: productId,
-          quantity: quantity
+          productId,
+          quantity
         });
       }
 
